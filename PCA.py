42659e014c0a9b10621201bbaf706beabb30b8ed
MSalwowski <maciej.salwowski@wp.pl>
2022-09-27 17:50:35 +0200

    PCA analysis

---
+++ PCA.py
@@ -0,0 +1,81 @@
+"""
+In order to obtain correct plots comment out the classification section in ExtractData.py
+"""
+
+from ExtractData import *
+import scipy.linalg as linalg
+import matplotlib.pyplot as plt
+
+Xf = X.astype(float)
+N,M = Xf.shape
+
+# =============================================================================
+# =============================================================================
+# variance explained by PCA
+# =============================================================================
+# =============================================================================
+
+# normalization
+Xf = Xf - np.ones((N,1))*Xf.mean(axis=0)
+Xf = Xf*(1/np.std(Xf,0))
+
+U,S,V = linalg.svd(Xf,full_matrices=False)
+
+rho = (S*S) / (S*S).sum() 
+
+threshold = 0.9
+
+plt.figure()
+plt.plot(range(1,len(rho)+1),rho,'x-')
+plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plt.plot([1,len(rho)],[threshold, threshold],'k--')
+plt.title('Variance explained by principal components');
+plt.xlabel('Principal component');
+plt.ylabel('Variance explained');
+plt.legend(['Individual','Cumulative','Threshold'])
+plt.grid()
+plt.show()
+
+# =============================================================================
+# =============================================================================
+# the ammount of variation explained as the number of PCA components included
+# =============================================================================
+# =============================================================================
+
+pcs = [[0,1,2],[3,4,5]]
+for pcs_triple in pcs:
+    legendStrs = ['PC'+str(e+1) for e in pcs_triple]
+    c = ['r','g','b']
+    bw = .2
+    r = np.arange(1,M+1)
+    for i in pcs_triple:    
+        plt.bar(r+i*bw, V[:,i], width=bw)
+        plt.xticks(r+bw, r)
+        # with attributes name (its unreadable):
+        # plt.xticks(r+bw, attributeNames[:])
+    plt.xlabel('Attribute index')
+    plt.ylabel('Component coefficients')
+    plt.legend(legendStrs)
+    plt.grid()
+    plt.title('Obesity: PCA Component Coefficients')
+    plt.show()
+
+# =============================================================================
+# =============================================================================
+# plot of PCA's against each other
+# =============================================================================
+# =============================================================================
+
+V = V.T
+Z = Xf @ V
+
+# component choice
+i = 0
+j = 1
+
+f = plt.figure()
+plt.title('Obesity data: PCA')
+plt.plot(Z[:,i], Z[:,j], 'o', alpha=.5)
+plt.xlabel('PC{0}'.format(i+1))
+plt.ylabel('PC{0}'.format(j+1))
+plt.show()
